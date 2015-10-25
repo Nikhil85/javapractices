c5ab94ced0136e1f732810828da57ec0eef56076
nikhil.sanghvi <nikhilssanghvi85@gmail.com>
2015-10-25 16:08:53 +0530

    Mongo DB integration with spring Boot

---
+++ gs-accessing-data-mongodb/src/main/java/org/springframework/data/mongo/SampleMongoApplication.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2012-2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.data.mongo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
+@PropertySource({"classpath:application.properties"})
+public class SampleMongoApplication implements CommandLineRunner {
+
+	@Autowired
+	private CustomerRepository repository;
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		repository.deleteAll();
+
+		// save a couple of customers
+		repository.save(new Customer("Alice", "Smith","retail",98));
+		repository.save(new Customer("Bob", "Smith","wholeseller",100));
+		repository.save(new Customer("san", "nik","stock",200));
+
+		// fetch all customers
+		System.out.println("Customers found with findAll():");
+		System.out.println("-------------------------------");
+		for (Customer customer : repository.findAll()) {
+			System.out.println(customer);
+		}
+		System.out.println();
+
+		// fetch an individual customer
+		System.out.println("Customer found with findByFirstName('Alice'):");
+		System.out.println("--------------------------------");
+		System.out.println(repository.findByFirstName("Alice"));
+
+		System.out.println("Customers found with findByLastName('Smith'):");
+		System.out.println("--------------------------------");
+		
+		System.out.println("Customers found with findByLastName('san'):");
+		System.out.println("--------------------------------");
+		for (Customer customer : repository.findByLastName("Smith")) {
+			System.out.println(customer);
+		}
+
+	}
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(SampleMongoApplication.class, args);
+	}
+}
